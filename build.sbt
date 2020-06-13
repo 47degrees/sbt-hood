1a802b32842a630c2df2cbf722462719cdcf6c67
Alejandro Hernández <info@alejandrohdezma.com>
2020-06-13 11:04:46 +0200

    Prepare repository for next `.github` release and SBT build improvements (#143)

--- build.sbt
+++ build.sbt
@@ -1,55 +1,26 @@
-addCommandAlias(
-  "ci-test",
-  "+scalafmtCheckAll; +scalafmtSbtCheck; +docs/mdoc; +coverage; +test; +coverageReport; +coverageAggregate"
-)
-addCommandAlias("ci-docs", "project-docs/mdoc; headerCreateAll")
-addCommandAlias("ci-microsite", "docs/publishMicrosite")
+ThisBuild / scalaVersion := "2.12.11"
+ThisBuild / organization := "com.47deg"
+
+addCommandAlias("ci-test", "scalafmtCheckAll; scalafmtSbtCheck; mdoc; testCovered")
+addCommandAlias("ci-docs", "github; mdoc; headerCreateAll; publishMicrosite")
+addCommandAlias("ci-publish", "github; ci-release")
+
+skip in publish := true
 
 lazy val `sbt-hood-core` = project
   .in(file("modules/core"))
-  .settings(moduleName := "sbt-hood-core")
 
 lazy val `sbt-hood-plugin` = project
   .in(file("modules/plugin"))
-  .dependsOn(`sbt-hood-core`)
-  .settings(moduleName := "sbt-hood-plugin")
-  .settings(sbtPluginSettings: _*)
-  .enablePlugins(BuildInfoPlugin)
-  .settings(buildInfoKeys := Seq[BuildInfoKey](name, version, scalaVersion, sbtVersion))
-  .settings(buildInfoPackage := "com.fortysevendeg.hood")
-  // See https://github.com/sbt/sbt/issues/3248
-  .settings(
-    publishLocal := publishLocal
-      .dependsOn(`sbt-hood-core` / publishLocal)
-      .value
-  )
-  .settings(sbtPlugin := true)
   .enablePlugins(SbtPlugin)
+  .dependsOn(`sbt-hood-core`)
 
-lazy val allModules: Seq[ProjectReference] = Seq(`sbt-hood-core`, `sbt-hood-plugin`)
-
-lazy val allModulesDeps: Seq[ClasspathDependency] =
-  allModules.map(ClasspathDependency(_, None))
-
-lazy val root = project
-  .in(file("."))
-  .settings(name := "sbt-hood")
-  .settings(skip in publish := true)
-  .aggregate(allModules: _*)
-  .dependsOn(allModulesDeps: _*)
-
-lazy val docs = project
-  .dependsOn(allModulesDeps: _*)
-  .settings(name := "sbt-hood-docs")
+lazy val microsite = project
+  .enablePlugins(MicrositesPlugin)
   .settings(micrositeSettings: _*)
   .settings(skip in publish := true)
-  .enablePlugins(MicrositesPlugin)
 
-lazy val `project-docs` = (project in file(".docs"))
-  .aggregate(allModules: _*)
-  .dependsOn(allModulesDeps: _*)
-  .settings(moduleName := "sbt-hood-project-docs")
-  .settings(mdocIn := file(".docs"))
+lazy val documentation = project
+  .enablePlugins(MdocPlugin)
   .settings(mdocOut := file("."))
   .settings(skip in publish := true)
-  .enablePlugins(MdocPlugin)
--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -1,10 +1,7 @@
 import com.typesafe.sbt.site.jekyll.JekyllPlugin.autoImport._
 import microsites.MicrositeKeys._
 import sbt.Keys._
-import sbt.ScriptedPlugin.autoImport._
 import sbt._
-import scoverage.ScoverageKeys._
-import com.alejandrohdezma.sbt.github.SbtGithubPlugin
 
 import scala.language.reflectiveCalls
 
@@ -12,43 +9,12 @@ object ProjectPlugin extends AutoPlugin {
 
   override def trigger: PluginTrigger = allRequirements
 
-  override def requires: Plugins = SbtGithubPlugin
-
   object autoImport {
-
-    lazy val V = new {
-      val scala: String          = "2.12.8"
-      val scalatest: String      = "3.1.2"
-      val slf4j: String          = "1.7.30"
-      val circe: String          = "0.13.0"
-      val http4s: String         = "0.21.4"
-      val github4s: String       = "0.24.1"
-      val cats: String           = "2.1.3"
-      val log4cats: String       = "1.1.1"
-      val logbackClassic: String = "1.2.3"
-      val kantan: String         = "0.6.1"
-      val console4cats: String   = "0.8.1"
-      val lightbendEmoji: String = "1.2.1"
-    }
-
-    lazy val sbtPluginSettings: Seq[Def.Setting[_]] = Seq(
-      scriptedLaunchOpts := {
-        scriptedLaunchOpts.value ++
-          Seq(
-            "-Xmx2048M",
-            "-XX:ReservedCodeCacheSize=256m",
-            "-XX:+UseConcMarkSweepGC",
-            "-Dversion=" + version.value
-          )
-      }
-    )
 
     lazy val micrositeSettings: Seq[Def.Setting[_]] = Seq(
       micrositeName := "SBT-Hood",
       micrositeBaseUrl := "/sbt-hood",
       micrositeDescription := "A SBT plugin for comparing benchmarks in your PRs",
-      micrositeGithubOwner := "47degrees",
-      micrositeGithubRepo := "sbt-hood",
       micrositeDocumentationUrl := "docs",
       micrositeGitterChannelUrl := "47deg/sbthood",
       micrositeOrganizationHomepage := "http://www.47deg.com",
@@ -60,42 +26,29 @@ object ProjectPlugin extends AutoPlugin {
         "brand-primary"   -> "#25bc77",
         "brand-secondary" -> "#25bc77",
         "white-color"     -> "#FFF"
-      ),
-      scalacOptions ~= (_ filterNot Set("-Ywarn-unused-import", "-Xlint").contains)
+      )
     )
 
   }
 
-  import autoImport._
-
   override def projectSettings: Seq[Def.Setting[_]] =
     Seq(
-      organization := "com.47deg",
-      scalaVersion := V.scala,
-      crossScalaVersions := Seq(V.scala),
-      Test / fork := true,
-      compileOrder in Compile := CompileOrder.JavaThenScala,
-      coverageFailOnMinimum := false,
-      resolvers ++= Seq(
-        Resolver.sonatypeRepo("releases"),
-        Resolver.sonatypeRepo("snapshots"),
-        Resolver.typesafeIvyRepo("releases")
-      ),
+      resolvers += Resolver.typesafeIvyRepo("releases"),
       libraryDependencies ++= Seq(
-        "io.circe"          %% "circe-generic"       % V.circe,
-        "io.circe"          %% "circe-core"          % V.circe,
-        "io.circe"          %% "circe-parser"        % V.circe,
-        "org.http4s"        %% "http4s-blaze-client" % V.http4s,
-        "com.47deg"         %% "github4s"            % V.github4s,
-        "org.typelevel"     %% "cats-effect"         % V.cats,
-        "io.chrisdavenport" %% "log4cats-slf4j"      % V.log4cats,
-        "ch.qos.logback"     % "logback-classic"     % V.logbackClassic,
-        "com.nrinaudo"      %% "kantan.csv"          % V.kantan,
-        "com.nrinaudo"      %% "kantan.csv-generic"  % V.kantan,
-        "dev.profunktor"    %% "console4cats"        % V.console4cats,
-        "com.lightbend"     %% "emoji"               % V.lightbendEmoji,
-        "org.scalatest"     %% "scalatest"           % V.scalatest % Test,
-        "org.slf4j"          % "slf4j-nop"           % V.slf4j     % Test
+        "io.circe"          %% "circe-generic"       % "0.13.0",
+        "io.circe"          %% "circe-core"          % "0.13.0",
+        "io.circe"          %% "circe-parser"        % "0.13.0",
+        "org.http4s"        %% "http4s-blaze-client" % "0.21.4",
+        "com.47deg"         %% "github4s"            % "0.24.1",
+        "org.typelevel"     %% "cats-effect"         % "2.1.3",
+        "io.chrisdavenport" %% "log4cats-slf4j"      % "1.1.1",
+        "ch.qos.logback"     % "logback-classic"     % "1.2.3",
+        "com.nrinaudo"      %% "kantan.csv"          % "0.6.1",
+        "com.nrinaudo"      %% "kantan.csv-generic"  % "0.6.1",
+        "dev.profunktor"    %% "console4cats"        % "0.8.1",
+        "com.lightbend"     %% "emoji"               % "1.2.1",
+        "org.scalatest"     %% "scalatest"           % "3.1.2"  % Test,
+        "org.slf4j"          % "slf4j-nop"           % "1.7.30" % Test
       )
     )
 }
