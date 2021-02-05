19e66c08d4ebf3da3568a55d8494b77e0b23e169
47erbot <developer@47deg.com>
2021-02-05 06:15:32 +0100

    Update http4s-blaze-client to 0.21.18

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -38,7 +38,7 @@ object ProjectPlugin extends AutoPlugin {
         "io.circe"          %% "circe-generic"       % "0.13.0",
         "io.circe"          %% "circe-core"          % "0.13.0",
         "io.circe"          %% "circe-parser"        % "0.13.0",
-        "org.http4s"        %% "http4s-blaze-client" % "0.21.17",
+        "org.http4s"        %% "http4s-blaze-client" % "0.21.18",
         "com.47deg"         %% "github4s"            % "0.28.1",
         "org.typelevel"     %% "cats-effect"         % "2.3.1",
         "io.chrisdavenport" %% "log4cats-slf4j"      % "1.1.1",
