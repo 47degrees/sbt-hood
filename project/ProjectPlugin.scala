da8a15d365a810e6056439c38e6460dd3b1bb22c
47erbot <developer@47deg.com>
2021-05-17 05:30:29 +0000

    Update scalatest to 3.2.9

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -47,7 +47,7 @@ object ProjectPlugin extends AutoPlugin {
         "com.nrinaudo"      %% "kantan.csv-generic"  % "0.6.1",
         "dev.profunktor"    %% "console4cats"        % "0.8.1",
         "com.lightbend"     %% "emoji"               % "1.2.3",
-        "org.scalatest"     %% "scalatest"           % "3.2.8"  % Test,
+        "org.scalatest"     %% "scalatest"           % "3.2.9"  % Test,
         "org.slf4j"          % "slf4j-nop"           % "1.7.30" % Test
       )
     )
