f4681f54e2717b452fc96fc7243aedac2514a4a5
fortyseven_degrees <developer@47deg.com>
2022-08-24 05:46:55 +0000

    Update emoji to 1.3.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -45,7 +45,7 @@ object ProjectPlugin extends AutoPlugin {
         "ch.qos.logback"         % "logback-classic"     % "1.2.11",
         "com.nrinaudo"          %% "kantan.csv"          % "0.6.2",
         "com.nrinaudo"          %% "kantan.csv-generic"  % "0.6.2",
-        "com.lightbend"         %% "emoji"               % "1.2.3",
+        "com.lightbend"         %% "emoji"               % "1.3.0",
         "com.github.marklister" %% "base64"              % "0.3.0",
         "org.scalatest"         %% "scalatest"           % "3.2.12" % Test,
         "org.typelevel"         %% "log4cats-noop"       % "2.4.0"  % Test
